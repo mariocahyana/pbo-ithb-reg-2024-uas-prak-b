54f45620d54f4dc34a32aa6fdb0855741240fdd6
mariocahyana <mariocahyana55@gmail.com>
2025-01-10 23:12:05 +0700

    minor bug

--- src/View/ViewHistoryView.java
+++ src/View/ViewHistoryView.java
@@ -26,18 +26,29 @@ public class ViewHistoryView {
 
         DesignPanel gradientPanel = new DesignPanel(new Color(0, 102, 204), new Color(102, 204, 255));
         gradientPanel.setLayout(new BorderLayout());
+        gradientPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         JLabel titleLabel = new JLabel("Menu History");
         titleLabel.setFont(new Font("SansSerif", Font.BOLD, 22));
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
         gradientPanel.add(titleLabel, BorderLayout.NORTH);
 
         int customerId = CustomerCheckSession.getInstance().getCustomer().getCustomer_id();
         DefaultTableModel model = historyController.getHistory(customerId);
 
         historyTable = new JTable(model);
+        historyTable.setBackground(Color.WHITE);
+        historyTable.setForeground(Color.BLACK);
+        historyTable.setGridColor(new Color(220, 220, 220));
+        historyTable.setRowHeight(25);
+        historyTable.getTableHeader().setBackground(new Color(0, 102, 204));
+        historyTable.getTableHeader().setForeground(Color.WHITE);
+        historyTable.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 12));
+
         JScrollPane scrollPane = new JScrollPane(historyTable);
+        scrollPane.setBorder(BorderFactory.createLineBorder(new Color(0, 102, 204)));
         gradientPanel.add(scrollPane, BorderLayout.CENTER);
 
         backButton = new DesignButton("Back", new Color(0, 102, 204), new Color(102, 204, 255));
@@ -47,6 +58,8 @@ public class ViewHistoryView {
         });
 
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        bottomPanel.setOpaque(false);
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         bottomPanel.add(backButton);
         gradientPanel.add(bottomPanel, BorderLayout.SOUTH);
 
